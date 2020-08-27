9a49f83f10d15f27108ccfa7c4a70dbc7a980b18
fanliangqin <fanliangqin@ultrain.io>
2020-08-27 15:25:01 +0800

    update contract

--- superhot-contract/contract/MyContract.ts
+++ superhot-contract/contract/MyContract.ts
@@ -5,59 +5,265 @@ import { Action } from 'ultrain-ts-lib/src/action';
 import { JSON } from 'ultrain-ts-lib/src/json';
 import { intToString } from 'ultrain-ts-lib/src/utils';
 import { Log } from 'ultrain-ts-lib/src/log';
+import { now } from 'ultrain-ts-lib/src/time';
 
 const SupplyTable = 'stat';
-const TicketsTable = 'ticket';
+const TicketsTable = 'ticket2';
 
-class SetableTicket implements Serializable {
+class IssueParams implements Serializable {
+  showName: string; //演出名称
+  showPlace: string; //演出地点
+  showTime: string; // 演出时间
+  issueTime: string;// 发行时间
+  issueAmount: u64;// 发行数量
+  highestPrice: u64; // 最高允许售价
+  lowestPrice: u64; // 最低允许售价
+  maxTransferCount: u64; // 最多允许转手次数
+  transRebates: u8[]; // 转手抽成比例
+
+  constructor() {
+    this.showName = '';
+    this.showPlace= '';
+    this.showTime = '';
+    this.issueTime = '';
+    this.issueAmount = 0;
+    this.highestPrice = 0;
+    this.lowestPrice = 0;
+    this.maxTransferCount = 0;
+    this.transRebates = [];
+  }
+
+  primaryKey(): u64 { return 0; }
+}
+
+class IssuedIds implements Serializable {
+  iids: u64[];
+
+  constructor() {
+    this.iids = [];
+  }
+
+  static add(id: u64): void {
+    let db = new DBManager<IssuedIds>(NAME('sii.ids'), NAME('sii.ids'));
+    let iids = new IssuedIds();
+    let existing = db.get(0, iids);
+    iids.iids.push(id);
+    if (existing) {
+      db.modify(iids);
+    } else {
+      db.emplace(iids);
+    }
+  }
+
+  static read(): u64[] {
+    let db = new DBManager<IssuedIds>(NAME('sii.ids'), NAME('sii.ids'));
+    let iids = new IssuedIds();
+    let existing = db.get(0, iids);
+    return existing ? iids.iids : [];
+  }
+
+  primaryKey(): u64 { return 0; }
+}
+
+class SavedIssueInfo implements Serializable, Returnable {
+  @primaryid
   id: u64;
-  title: string; // 票的描述
-  highestPrice: u64; // 最高售价, 最后4位为小数部分
-  rebate: u64;  // 折扣, 设置为一个整数, 表示百分比, 如30表示 30%
-  commission: u64; // 佣金比例, 设置为一个整数, 表示百分比, 同rebate
+  issueParmas: IssueParams;
+  availableNfts: u64[]; // 可以发行的NFT
 
   constructor() {
     this.id = 0;
-    this.title = '';
-    this.highestPrice = 0;
-    this.rebate = 0;
-    this.commission = 0;
+    this.issueParmas = new IssueParams();
+    this.availableNfts = [];
+  }
+
+  nftIds(): string {
+    let s = '[';
+    for (let i = 0; i < this.availableNfts.length; i++) {
+      s += intToString(this.availableNfts[i]);
+      if (i < this.availableNfts.length - 1) {
+        s += ',';
+      }
+    }
+    s += ']';
+    return s;
+  }
+
+  toString(): string {
+    let json = new JSON();
+    json.item<u64>('issueId', this.id);
+    json.item<string>('issueParams', this.issueParmas.toString());
+    json.item<string>('availalbeNfts', this.nftIds());
+    return json.toString();
+  }
+
+  private static SIIDB(): DBManager<SavedIssueInfo> {
+    return new DBManager<SavedIssueInfo>(NAME('sii'), NAME('sii'));
+  }
+
+  private static NftIdPrefix(): u64 {
+
+      let nft: u64 = 0xA000000000000000;
+      let ti: u64 = (<u64>now() << 28);
+      return nft + ti;
+  }
+
+  static add(param: IssueParams): SavedIssueInfo {
+    let sii = new SavedIssueInfo();
+    sii.id = U64IdGenerator.nextId();
+    sii.issueParmas = param;
+    let idprefix = SavedIssueInfo.NftIdPrefix();
+    for (let i = 0; i < param.issueAmount; ++i) {
+      sii.availableNfts.push(idprefix + i);
+    }
+
+    let db = SavedIssueInfo.SIIDB();
+    db.emplace(sii);
+
+    IssuedIds.add(sii.id);
+
+    return sii;
+  }
+
+  static query(id: u64): SavedIssueInfo {
+    let sii = new SavedIssueInfo();
+    let db = SavedIssueInfo.SIIDB();
+    let existing = db.get(id, sii);
+    ultrain_assert(existing, 'issue id ' + intToString(id) + ' is not exist.');
+    return sii;
+  }
+
+  static allIds(): u64[] {
+    return IssuedIds.read();
+  }
+}
+
+class TicketTransferRecord  implements Serializable, Returnable {
+  ticketPrice: u64; // 票价
+
+  buyerFee: u64; // 买方手续费
+  buyerPaied: u64; // 买方实付金额
+
+  sellerFee: u64; // 卖方手续费
+  sellerIncoming: u64; // 卖方实际收入
+
+  sellerAccount: string; // 卖方账号
+  buyerAccount: string; // 买方账号
+
+  time: string; // 交易时间
+  status: string; // 交易状态
+  txId: string; // 交易id
+
+  constructor() {
+    this.ticketPrice = 0;
+    this.buyerFee = 0;
+    this.buyerPaied = 0;
+    this.sellerFee = 0;
+    this.sellerIncoming = 0;
+    this.sellerAccount = '';
+    this.buyerAccount = '';
+    this.time = '';
+    this.status = '';
+    this.txId = '';
+  }
+
+  toString(): string {
+    let json = new JSON();
+    json.item<u64>('ticketPrice', this.ticketPrice);
+    json.item<u64>('buyerFee', this.buyerFee);
+    json.item<u64>('buyerPaied', this.buyerPaied);
+    json.item<u64>('sellerFee', this.sellerFee);
+    json.item<u64>('sellerIncoming', this.sellerIncoming);
+    json.item<string>('buyer', this.buyerAccount);
+    json.item<string>('seller', this.sellerAccount);
+    json.item<string>('time', this.time);
+    json.item<string>('status', this.status);
+    json.item<string>('txId', this.txId);
+    return json.toString();
+  }
+
+  primaryKey(): u64 { return 0; }
+}
+
+class TicketFirstSell implements Serializable, Returnable {
+  price: u64; // 交易金额
+  platformFee: u64; // 平台手续费
+  sellerIncoming: u64; // 卖方收入
+  seller: string; // 卖方账号
+  buyer: string; // 买方账号
+  time: string; // 时间
+  status: string; // 状态
+  txId: string;
+
+  constructor() {
+    this.price = 0;
+    this.platformFee = 0;
+    this.sellerIncoming = 0;
+    this.seller = '';
+    this.buyer = '';
+    this.time = '';
+    this.status = '';
+    this.txId = '';
+  }
+
+  toString(): string {
+    let json = new JSON();
+    json.item<u64>('price', this.price);
+    json.item<u64>('platformFee', this.platformFee);
+    json.item<u64>('sellerIncoming', this.sellerIncoming);
+    json.item<string>('seller', this.seller);
+    json.item<string>('buyer', this.buyer);
+    json.item<string>('time', this.time);
+    json.item<string>('status', this.status);
+    json.item<string>('txId', this.txId);
+    return json.toString();
+  }
+
+  primaryKey(): u64 {
+    return 0;
   }
 }
 
 class Ticket implements Serializable, Returnable {
   @primaryid
-  id: u64;
-  title: string; // 票的描述
-  highestPrice: u64; // 最高售价, 最后4位为小数部分
-  transferCount: u64; // 转让次数
-  rebate: u64;  // 折扣, 设置为一个整数, 表示百分比, 如30表示 30%
-  commission: u64; // 佣金比例, 设置为一个整数, 表示百分比, 同rebate
+  nft: u64; // 票的Nft编号
+  issueId: u64; // 发行id
 
-  owner: account_name; // 拥有者
-  prevOwner: account_name; // 上一个拥有者
+  showName: string;
+  showPlace: string;
+
+  firstSell: TicketFirstSell;
+  transferRecord: TicketTransferRecord[];
 
   constructor() {
-    this.id = 0;
-    this.title = '';
-    this.highestPrice = 0;
-    this.transferCount = 0;
-    this.rebate = 0;
-    this.commission = 0;
-    this.owner = 0;
-    this.prevOwner = 0;
+    this.nft= 0;
+    this.issueId= 0;
+    this.showName = '';
+    this.showPlace = '';
+    this.firstSell = new TicketFirstSell();
+    this.transferRecord = [];
+  }
+
+  transferRecordString(): string {
+    let s = '[';
+    for (let i = 0; i < this.transferRecord.length; ++i) {
+      s += this.transferRecord[i].toString();
+      if (i < this.transferRecord.length - 1)
+      s += ','
+    }
+    s += ']';
+
+    return s;
   }
 
   toString(): string {
     let json = new JSON();
-    json.item<u64>('id', this.id);
-    json.item<string>('title', this.title);
-    json.item<u64>('highest-price', this.highestPrice);
-    json.item<u64>('transfer-count', this.transferCount);
-    json.item<u64>('rebate', this.rebate);
-    json.item<u64>('commission', this.commission);
-    json.item<string>('owner', RNAME(this.owner));
-    json.item<string>('previous-owner', RNAME(this.prevOwner));
+    json.item<u64>('nft', this.nft);
+    json.item<u64>('issueId', this.issueId);
+    json.item<string>('showName', this.showName);
+    json.item<string>('showPlace', this.showPlace);
+    json.item<string>('firstSell', this.firstSell.toString());
+    json.item<string>('transferRecord', this.transferRecordString());
     return json.toString();
   }
 
@@ -65,113 +271,45 @@ class Ticket implements Serializable, Returnable {
     return new DBManager<Ticket>(NAME(TicketsTable), NAME(TicketsTable));
   }
 
-  static add(from: account_name, to: account_name, tickets: SetableTicket[]): u64[] {
-    let tdb = Ticket.TicketsDbManager();
-    let tids: u64[] = [];
-    for (let i = 0; i < tickets.length; i++) {
-      let ticket = new Ticket();
-      ticket.title = tickets[i].title;
-      ticket.highestPrice = tickets[i].highestPrice;
-      ticket.commission = tickets[i].commission;
-      ticket.rebate = tickets[i].rebate;
-
-      ticket.id = TicketIdGenerator.nextId();
-      ticket.transferCount = 0;
-      ticket.owner = to;
-      ticket.prevOwner = from;
-
-      tdb.emplace(ticket);
-
-      tids.push(ticket.id);
-    }
-
-    return tids;
-  }
-
-  static update(ticket: SetableTicket): void {
-    let tdb = Ticket.TicketsDbManager();
-    let storedTicket = new Ticket();
-    let existing = tdb.get(ticket.id, storedTicket);
-    ultrain_assert(existing, 'This ticket is not existing.');
-    ultrain_assert(Action.sender === storedTicket.owner, 'you are not the owner of this ticket: ' + intToString(ticket.id));
-
-    storedTicket.title = ticket.title;
-    storedTicket.highestPrice = ticket.highestPrice;
-    storedTicket.rebate = ticket.rebate;
-    storedTicket.commission = ticket.commission;
-
-    tdb.modify(storedTicket);
-  }
-
-  static transfer(from: account_name, to: account_name, tids: u64[]): u64[] {
+  static issue(ticket: Ticket): void {
     let db = Ticket.TicketsDbManager();
-    let failedIds: u64[] = [];
-    for (let i = 0; i < tids.length; ++i) {
-      let ticket = new Ticket();
-      let existing = db.get(tids[i], ticket);
-      if (existing && ticket.owner === from) {
-        ++ticket.transferCount;
-        ticket.owner = to;
-        ticket.prevOwner = from;
-        db.modify(ticket);
-      } else {
-        failedIds.push(tids[i]);
-      }
-    }
-    return failedIds;
+    db.emplace(ticket);
   }
 
-  static getTickets(tids: u64[]): Ticket[] {
-    let tickets: Ticket[] = [];
+  static transfer(nft: u64, transferRecord: TicketTransferRecord): void {
     let db = Ticket.TicketsDbManager();
-    for (let i = 0; i < tids.length; i++) {
-      let t = new Ticket();
-      let existing = db.get(tids[i], t);
-      if (existing) {
-        tickets.push(t);
-      }
-    }
+    let ticket = new Ticket();
+    let existing = db.get(nft, ticket);
+    ultrain_assert(existing, 'nft ' + intToString(nft) + ' is not exist.');
 
-    return tickets;
+    let sid = ticket.issueId;
+    let sii = SavedIssueInfo.query(sid);
+    ultrain_assert(sii.issueParmas.maxTransferCount > ticket.transferRecord.length, 'Can not transfer this ticket, reach max transfer count.');
+
+    ticket.transferRecord.push(transferRecord);
+    db.modify(ticket);
+  }
+
+  static firstSell(nft: u64, sellInfo: TicketFirstSell): void {
+    let db = Ticket.TicketsDbManager();
+    let ticket = new Ticket();
+    let existing = db.get(nft, ticket);
+    ultrain_assert(existing, 'nft ' + intToString(nft) + ' not exist.');
+
+    ticket.firstSell = sellInfo;
+    db.modify(ticket);
+  }
+
+  static query(nft: u64): Ticket {
+    let db = Ticket.TicketsDbManager();
+    let t = new Ticket();
+    let existing = db.get(nft, t);
+    ultrain_assert(existing, 'nft ' + intToString(nft) + ' is not exist.');
+    return t;
   }
 }
 
-class SupplyInfo implements Serializable {
-  supply: Asset;
-  max_supply: Asset;
-  issuer: account_name;
-
-  constructor() {
-      this.max_supply = new Asset();
-      this.supply = new Asset();
-      this.issuer = 0;
-  }
-
-  // @ts-ignore
-  newInstance(supply: Asset, max_supply: Asset, issuer: account_name): SupplyInfo {
-      var stats = new SupplyInfo();
-      stats.max_supply = max_supply;
-      stats.supply = supply;
-      stats.issuer = issuer;
-  }
-
-  primaryKey(): id_type { return this.supply.symbolName(); }
-
-  static addIssuerInfo(issuer: account_name, maximum_supply: Asset): void {
-    let statstable: DBManager<SupplyInfo> = new DBManager<SupplyInfo>(NAME(SupplyTable), NAME(SupplyTable));
-    let cs: SupplyInfo = new SupplyInfo();
-    let sym = maximum_supply.symbolName();
-    let existing = statstable.get(sym, cs);
-    ultrain_assert(!existing, "token with symbol already exists.");
-
-    cs.supply.setSymbol(maximum_supply.getSymbol());
-    cs.max_supply = maximum_supply;
-    cs.issuer = issuer;
-    statstable.emplace(cs);
-  }
-}
-
-class TicketIdGenerator implements Serializable {
+class U64IdGenerator implements Serializable {
   index: u64;
   constructor() {
     this.index = 0;
@@ -182,8 +320,8 @@ class TicketIdGenerator implements Serializable {
   }
 
   static nextId(): u64 {
-    let tiddb = new DBManager<TicketIdGenerator>(NAME('ticket.id'), NAME('ticket.id'));
-    let tid = new TicketIdGenerator();
+    let tiddb = new DBManager<U64IdGenerator>(NAME('ticket.id'), NAME('ticket.id'));
+    let tid = new U64IdGenerator();
     let existing = tiddb.get(0, tid);
     tid.index = tid.index + 1;
 
@@ -196,110 +334,51 @@ class TicketIdGenerator implements Serializable {
   }
 }
 
-class OwnershipInfo implements Serializable {
-  @primaryid
-  owner: account_name;
-  ids: u64[];
-
-  constructor() {
-    this.owner = 0;
-    this.ids = [];
-  }
-
-  private static ownershipDb(): DBManager<OwnershipInfo> {
-    return new DBManager<OwnershipInfo>(NAME('ownership'), NAME('ownership'));
-  }
-
-  static ticketsOf(owner: account_name): u64[] {
-    let db = OwnershipInfo.ownershipDb();
-    let oi = new OwnershipInfo();
-    let existing = db.get(owner, oi);
-    if (existing) { return oi.ids; }
-    else {return []; }
-  }
-
-  static addTickets(owner: account_name, ids: u64[]): void {
-    let db = OwnershipInfo.ownershipDb();
-    let oi = new OwnershipInfo();
-    let existing = db.get(owner, oi);
-
-    if (existing) {
-      oi.ids = oi.ids.concat(ids);
-      db.modify(oi);
-    } else {
-      oi.owner = owner;
-      oi.ids = ids;
-      db.emplace(oi);
-
-    }
-  }
-
-  static removeTickets(owner: account_name, ids: u64[]): void {
-    let db = OwnershipInfo.ownershipDb();
-    let oi = new OwnershipInfo();
-    let existing = db.get(owner, oi);
-
-    ultrain_assert(existing, RNAME(owner) + ' owns nothing.');
-
-    for (let i = oi.ids.length - 1; i >= 0; i--) {
-      if (ids.indexOf(oi.ids[i]) != -1) {
-        oi.ids.splice(i, 1);
-      }
-    }
-
-    db.modify(oi);
-  }
-}
-
-@database(SupplyInfo, SupplyTable)
 @database(Ticket, TicketsTable)
 class MyContract extends Contract {
-  // 发行方, 设置最高的发行数量
   @action
-  create(issuer: account_name, maximum_supply: Asset): void {
+  issueTickets(issueInfo: IssueParams): u64 {
     Action.requireAuth(this.receiver);
-    ultrain_assert(maximum_supply.isSymbolValid(), "token.create: invalid symbol name.");
-    ultrain_assert(maximum_supply.symbolPrecision() == 0, "token.create: symbol precision must be a whole number");
-    ultrain_assert(maximum_supply.isValid(), "token.create: invalid supply.");
+    let sii = SavedIssueInfo.add(issueInfo);
 
-    SupplyInfo.addIssuerInfo(issuer, maximum_supply);
+    for (let i = 0; i < sii.availableNfts.length; ++i) {
+      let ticket = new Ticket();
+      ticket.nft = sii.availableNfts[i];
+      ticket.issueId = sii.id;
+      ticket.showName = sii.issueParmas.showName;
+      ticket.showPlace = sii.issueParmas.showPlace;
+
+      Ticket.issue(ticket);
+    }
+    return sii.id;
   }
-  // 承销方, 最多拥有的票数
+
   @action
-  issue(to: account_name, tickets: SetableTicket[]): void {
+  firstSellTicket(nft: u64, sellinfo: TicketFirstSell): void {
     Action.requireAuth(this.receiver);
-    ultrain_assert(Account.isValid(to), 'Account ' + RNAME(to) + ' is not exist.');
-
-    let tids = Ticket.add(this.receiver, to, tickets);
-    OwnershipInfo.addTickets(to, tids);
+    Ticket.firstSell(nft, sellinfo);
   }
 
-  // 设置票的信息
   @action
-  setTicketInfo(ticket: SetableTicket): void {
-    Ticket.update(ticket);
-  }
-
-  // 转让
-  @action
-  transfer(from: account_name, to: account_name, tokenIds: u64[], memo: string): void {
-    Action.requireAuth(from);
-    ultrain_assert(from !== to, 'you can not transfer to yourself.');
-
-    OwnershipInfo.removeTickets(from, tokenIds);
-    OwnershipInfo.addTickets(to, tokenIds);
-
-    Ticket.transfer(from, to, tokenIds);
+  transferTicket(nft: u64, ticket: TicketTransferRecord): void {
+    Action.requireAuth(this.receiver);
+    Ticket.transfer(nft, ticket);
   }
 
   @action("pureview")
-  balanceOf(owner: account_name): Ticket[] {
-    let tids = OwnershipInfo.ticketsOf(owner);
-    return Ticket.getTickets(tids);
+  issuedIds(): u64[] {
+    return SavedIssueInfo.allIds();
   }
 
   @action("pureview")
-  detailsOf(ticketIds: u64[]): Ticket[] {
-    return Ticket.getTickets(ticketIds);
+  issueInfo(issueId: u64): SavedIssueInfo {
+    let sii = SavedIssueInfo.query(issueId);
+    return sii;
+  }
+
+  @action("pureview")
+  ticketInfo(nft: u64): Ticket {
+    let t = Ticket.query(nft);
+    return t;
   }
 }
